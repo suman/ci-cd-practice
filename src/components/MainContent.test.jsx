30d657c604796f340a17da13848a74ab4dff2e2b
Suman Bogati <sumanbogati@gmail.com>
2023-10-07 19:48:00 +0530

    Check unit testing failed with github action

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -6,7 +6,7 @@ import MainContent from './MainContent';
 
 describe('MainContent', () => {
   it('should render a button', () => {
-    render(<MainContent />);
+    render(<div ></div>);
 
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
